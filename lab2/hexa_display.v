06d6ff3dbe0f45127f659b449b00304077e1b546
bowenzheng <dr.zheng49@gmail.com>
2018-09-25 23:44:26 -0400

    still wont work?

--- lab2/hexa_display.v
+++ lab2/hexa_display.v
@@ -2,59 +2,46 @@ module hexa_display(SW,HEX0);
 	input [3:0] SW;
 	output [6:0] HEX0;
 	
-	zero mod0(
+	helper h0(
 		.a(SW[0]),
 		.b(SW[1]),
 		.c(SW[2]),
 		.d(SW[3]),
-		.m(HEX0[0])
-		);
-
-	one mod1(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[1])
-		);
-
-	two mod2(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[2])
-		);
-	three mod3(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[3])
-		);
-   four mod4(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[4])
-		);
-	five mod5(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[5])
-		);
-	six mod6(
-		.a(SW[0]),
-		.b(SW[1]),
-		.c(SW[2]),
-		.d(SW[3]),
-		.m(HEX0[6])
-		);
+		.HEX0(HEX0[0]),
+		.HEX1(HEX0[1]),
+		.HEX2(HEX0[2]),
+		.HEX3(HEX0[3]),
+		.HEX4(HEX0[4]),
+		.HEX5(HEX0[5]),
+		.HEX6(HEX0[6])
+	);
 endmodule
 
+
+module helper(a,b,c,d,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6);
+	input a;
+	input b;
+	input c;
+	input d;
+	output HEX0;
+	output HEX1;
+	output HEX2;
+	output HEX3;
+	output HEX4;
+	output HEX5;
+	output HEX6;
+
+	assign HEX0 = ~a&~b&~c&d|~a&b&~c&~d|a&b&~c&d|a&~b&c&d;
+	assign HEX1 = ~a&b&~c&d|a&b&~d|a&c&d|b&c&~d;
+	assign HEX2 = a&b&~c&~d|a&b&c|~a&~b&c&~d;
+	assign HEX3 = ~a&~b&~c&d|~a&b&~c&~d|b&c&d|a&~b&c&~d;
+	assign HEX4 = ~a&d|~a&b&~c&~d|a&~b&~c&d;
+	assign HEX5 = ~a&~b&d|~a&b&c&d|a&b&~c&d|~a&~b&c&~d;
+	assign HEX6 = ~a&~b&~c|a&b&~c&~d|~a&b&c&d;
+
+endmodule
+
+/*
 module zero(a,b,c,d,m);
 	input a;
 	input b;
@@ -124,3 +111,4 @@ module six(a,b,c,d,m);
 	
 	assign m = ~a&~b&~c|a&b&~c&~d|~a&b&c&d;
 endmodule
+*/
