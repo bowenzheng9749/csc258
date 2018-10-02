b3015490582ebcf1cb33907b28cb4c63236a3998
bowenzheng <dr.zheng49@gmail.com>
2018-10-01 20:36:14 -0400

    trying to finish lab3 part2

---
+++ lab3/lab3part2.v
@@ -0,0 +1,24 @@
+
+
+module lab3part2(SW,LEDR);
+	input [8:0] SW; //SW[7:4] for A, SW[3:0] for B, SW[8] for cin. 
+	output [4:0] LEDR; //
+
+	wire c1, c2, c3;
+
+	fulladder FA1(SW[0],SW[4],SW[8], c1, LEDR[0]);
+	fulladder FA2(SW[1],SW[5],c1, c2, LEDR[1]);
+	fulladder FA3(SW[2],SW[6],c2, c3, LEDR[2]);
+	fulladder FA3(SW[3],SW[7],c3, LEDR[4],LEDR[3]);
+
+endmodule
+
+
+module fulladder(a,b,c,x,y);
+	input a,b,c;
+	output x; // this is for carry
+	output y; // this is for output
+
+	assgin x = (a&b)|(a&c)|(b&c)|(a&b&c);
+	assign y = a^b^c;
+endmodule
