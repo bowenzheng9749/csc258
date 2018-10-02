2f780db1c5b87804b8bad9c3eeb67b75cf97836c
bowenzheng <dr.zheng49@gmail.com>
2018-10-02 15:01:54 -0400

    Revert "add all the changes here"
    This reverts commit 0abe4b0f844ad9efaf95b15f654bf5cc1c2bbcaf.

---
+++ lab3/lab3part3.v
@@ -0,0 +1,21 @@
+module lab3part2(SW, LEDR, HEX0, HEX1, HEX3, HEX3, HEX4, HEX5);
+	input [7:0] SW;
+	input [2:0] KEY;
+	output [7:0] LEDR;
+	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+
+	wire [7:0] out;
+
+
+endmodule
+
+module aluFunctions(A, B, Function, aluOut);
+	input [3:0] A;
+	input [3:0] B;
+	input [3:0] Functions;
+	output aluOut;
+
+	lab3part2 a0 (
+
+
+	)
