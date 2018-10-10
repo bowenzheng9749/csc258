dc30b6a418e11ca10789c0f8df74a9959edad28b
bowenzheng <dr.zheng49@gmail.com>
2018-10-10 19:32:15 -0400

    a lot of modification

--- lab4/part3/lab4part3.v
+++ lab4/part3/lab4part3.v
@@ -13,25 +13,18 @@ module shifter(asr_in, shift, load_val, load_n, clk, reset_n, Q);
 	input shift;
 	input [7:0] load_val;
 	output [7:0] Q;
-	wire w7, w6, w5, w4, w3, w2, w1, W0;
+	wire w;
 
-	mux2to1 asr(1'b0, Q[7], asr_in, w7);
+	mux2to1 asr(1'b0, Q[7], asr_in, w);
 
-	shifterBit s7(w7, shift, load_val[7], load_n, clk, reset_n, w6);
-	shifterBit s6(w6, shift, load_val[6], load_n, clk, reset_n, w5);
-	shifterBit s5(w5, shift, load_val[5], load_n, clk, reset_n, w4);
-	shifterBit s4(w4, shift, load_val[4], load_n, clk, reset_n, w3);
-	shifterBit s3(w3, shift, load_val[3], load_n, clk, reset_n, w2);
-	shifterBit s2(w2, shift, load_val[2], load_n, clk, reset_n, w1);
-	shifterBit s1(w1, shift, load_val[1], load_n, clk, reset_n, w0);
-	shifterBit s0(w0, shift, load_val[0], load_n, clk, reset_n, Q[0]);
-	assign w6 = Q[7];
-	assign w5 = Q[6];
-	assign w4 = Q[5];
-	assign w3 = Q[4];
-	assign w2 = Q[3];
-	assign w1 = Q[2];
-	assign w0 = Q[1];
+	shifterBit s7(w, shift, load_val[7], load_n, clk, reset_n, Q[7]);
+	shifterBit s6(Q[7], shift, load_val[6], load_n, clk, reset_n, Q[6]);
+	shifterBit s5(Q[6], shift, load_val[5], load_n, clk, reset_n, Q[5]);
+	shifterBit s4(Q[5], shift, load_val[4], load_n, clk, reset_n, Q[4]);
+	shifterBit s3(Q[4], shift, load_val[3], load_n, clk, reset_n, Q[3]);
+	shifterBit s2(Q[3], shift, load_val[2], load_n, clk, reset_n, Q[2]);
+	shifterBit s1(Q[2], shift, load_val[1], load_n, clk, reset_n, Q[1]);
+	shifterBit s0(Q[1], shift, load_val[0], load_n, clk, reset_n, Q[0]);
 
 endmodule
 
