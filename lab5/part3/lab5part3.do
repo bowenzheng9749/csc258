d0472955219b50b853b9fec1d60318fa2c05b1ea
bowenzheng <dr.zheng49@gmail.com>
2018-10-16 22:13:06 -0400

    gave up

--- lab5/part3/lab5part3.do
+++ lab5/part3/lab5part3.do
@@ -1,21 +1,18 @@
 vlib work
 
-vlog -timescale 1ns/1ns lab5part3.v
+vlog -timescale 1ns/1ns morsecode.v
 
-vsim lab5part3
+vsim morsecode
 
+# Log all signals and add some signals to waveform window.
 log {/*}
-
+# add wave {/*} would add all items in top level simulation module.
 add wave {/*}
-
-force {SW[2:0]} 3'b000
-
-
+force {SW[2:0]} 3'b001
+# enable
+force {SW[3]} 0 0, 1 2
 # reset_n
 force {KEY[0]} 0 0, 1 2
-
 force {KEY[1]} 1 0, 0 1, 1 2, 0 3, 1 4
-
 force {CLOCK_50} 0 0, 1 1 -r 2
-
 run 200000010ns
--- lab5/part3/lab5part3.v
+++ lab5/part3/lab5part3.v
@@ -1,91 +1,105 @@
-module lab5part3(SW, KEY, CLOCK_50, LEDR);
-	input [2:0] SW;
+module morsecode(SW, KEY,CLOCK_50,LEDR0);
+	input [3:0] SW; // SW[2:0] selcet, SW[3] enable for rate divider
 	input [1:0] KEY;
 	input CLOCK_50;
-	output [1:0] LEDR;
-
-	morseencode m(SW[2:0], KEY[0], CLOCK_50, KEY[1], LEDR[0]);
+	output LEDR0;
+	
+	wire [13:0] load;
+	wire en;
+	
+	LUT l0(
+		.select(SW[2:0]),
+		.out(load)
+		);
+		
+	RateDivider r0 (
+		.enable(SW[3]),
+		.clock(CLOCK_50),
+		.reset_n(KEY[1]),
+		.q(en)
+		);
+		
+	ShifterRegister s0 (
+		.en(en),
+		.load(load),
+		.par_load(KEY[1]),
+		.clk(CLOCK_50),
+		.clear_b(KEY[0]),
+		.out(LEDR0)
+		);
 
 endmodule
 
-
-module morseencode(in, reset, clk, par_load, out);
-	input [2:0] in;
-	input reset;
-	input par_load;
-	input clk;
-	output out;
-
-	wire [13:0] w1;
-	wire w2;
-	lut l(in, w1);
-	shifter s(reset,w2,par_load, w1, out);
-
-	ratedivider r(reset, clk, w2);
-endmodule
-
-module lut(key, out);
-	input [2:0] key;
+module LUT(select, out);
+	input [2:0] select;
 	output reg [13:0] out;
 
 	always @(*)
 	begin
-		case(key)
-			3'd0: out = {8'd0, 6'b101010};
-			3'd1: out = {10'd0, 4'b1110};
-			3'd2: out = {6'd0, 8'b10101110};
-			3'd3: out = {4'd0, 10'b1010101110};
-			3'd4: out = {4'd0, 10'b1011101110};
-			3'd5: out = {2'd0, 12'b111010101110};
-			3'd6: out = {14'b11101011101110};
-			3'd7: out = {2'd0, 12'b111011101010};
+		case (select)
+			3'b000: out = 14'b10_1010_0000_0000;
+			3'b001: out = 14'b11_1000_0000_0000;
+			3'b010: out = 14'b10_1011_1000_0000;
+			3'b011: out = 14'b10_1010_1110_0000;
+			3'b100: out = 14'b10_1110_1110_0000;
+			3'b101: out = 14'b11_1010_1011_1000;
+			3'b110: out = 14'b11_1010_1110_1110;
+			3'b111: out = 14'b11_1011_1010_1000;
 		endcase
 	end
+	
 endmodule
 
-module shifter(reset_n, clock, par_load, load, out);
-	input reset_n, par_load, clock;
-	input [13:0] load;
-	output reg out;
-
-	reg [13:0] q;
-
-	always @(posedge clock, negedge reset_n, negedge par_load)
-		begin
-			if (reset_n == 0)
-				q <= 14'b00000000000000;
-			else if (par_load == 1'b0)
-				q <= load;
-				out <= q[13];
-				q <= q << 1'b1;
-		end
-
-endmodule
-
-
-module ratedivider(reset_n, clock, q);
-	input reset_n;
-	input clock;
+module RateDivider(enable,clock, reset_n,q);
+	input enable,clock,reset_n;
 	output q;
-
-	reg [27:0] rate;
-
-
-	always @(posedge clock, negedge reset_n)
+	
+	wire [24:0] load;
+	assign load = 25'd24_999_999;
+	
+	reg [24:0] out;
+	
+	always@(posedge clock)
 	begin
 		if (reset_n == 1'b0)
-
-			rate <= 0;
-
-		else if (rate == 0)
-
-			rate <= 28'd24999999;
-
-		else
-
-			rate <= rate - 1'b1;	
+			out <= load;
+		else if (enable == 1'b1)
+			begin
+				if (out == 25'd0)
+					out <= load;
+				else
+					out <= out - 1'b1;
+			end
 	end
+	
+	assign q = (out == 25'd0) ? 1 : 0;
+endmodule
 
-	assign q = (rate == 0) ? 1 : 0;
-
+module ShifterRegister(en,load,par_load,clk,clear_b,out);
+	input en, clk,clear_b,par_load;
+	input [13:0] load;
+	output reg out;
+	
+	reg [13:0] shifter;
+	
+	always @(posedge clk, negedge clear_b)
+	begin
+		if (clear_b == 1'b0)
+			begin
+				out <= 0;
+				shifter <= 0;
+			end
+		else if (par_load == 1'b0)
+			begin
+				shifter <= load;
+				out <= 0;
+			end
+		else if (en == 1'b1)
+			begin
+				out <= shifter[13];
+				shifter <= shifter << 1'b1;
+			end
+	end
+	
+	
 endmodule
