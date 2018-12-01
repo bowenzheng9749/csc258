3e143dcced28c64bd6387916042c0ce50bfb0ce1
bowenzheng <dr.zheng49@gmail.com>
2018-12-01 00:24:30 -0500

    more mod

--- project/random generatin/random.do
+++ project/random generatin/random.do
@@ -2,13 +2,13 @@ vlib work
 
 vlog -timescale 1ns/1ns random.v
 
-vsim rand
+vsim random
 
 log {/*}
 
 add wave {/*}
 
 force {clock} 0 0,1 5 -r 10
-force {give_random} 0 0, 1 25 -r 50
+force {enable} 0 0, 1 25 -r 50
 force {reset} 1 0,0 10
 run 20000ns
--- project/random generatin/random.v
+++ project/random generatin/random.v
@@ -1,43 +1,24 @@
-module rand (
-    input give_random,
-    input clock,
-    input reset,
-    output [7:0] rnd 
-    );
+module random (enable, clock, reset);
 
-reg [7:0] random, random_next, random_done;
+ouput [7:0] out;
+input clock, reset;
+input enable;
 
-wire feedback = random[7] ^ random[3] ^ random[2] ^ random[0]; 
- 
+reg [7:0] out;
 
+wire linear_feedback;
 
- 
-always @ (posedge clock)
+assign linear_feedback = !(out[7] ^ out[3]);
+
+always @(posedge clock)
 begin
- if (!reset)
- begin
-  random <= 8'hF; 
-
- end
-  
- else
- begin
-  random <= random_next;
- end
-end
- 
-always @ (*)
-begin
- random_next = random; //default state stays the same  
-  random_next = {random[6:0], feedback}; //shift left the xor'd every posedge clock
- 
- if (give_random)
- begin
-  random_done = random; //assign the random number to output
- end
+  if (!reset)
+    out <= 8'b0;
+  else if (enable)
+    begin
+      out <= {out[6],out[5],out[4],out[3],out[2],out[1],out[0],linear_feedback};
+    end
 end
 
-assign rnd = random_done;
 
 endmodule
- 
