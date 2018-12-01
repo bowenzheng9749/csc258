77b4a7caaa89d30ad5644059f192d6c05c6716c5
bowenzheng <dr.zheng49@gmail.com>
2018-12-01 00:41:54 -0500

    more mod

--- project/random generatin/random.v
+++ project/random generatin/random.v
@@ -8,7 +8,7 @@ output reg [7:0] out;
 wire linear_feedback;
 
 assign linear_feedback = out[7] ^ out[3] ^ out[2] ^ out[0]; 
-always @(posedge clock, posedge enable)
+always @(posedge enable)
 begin
   if (!reset)
     out <= 8'hF;
