fcc4558e842703c0677f9498bca59f2694724cfa
bowenzheng <dr.zheng49@gmail.com>
2018-12-01 00:00:43 -0500

    hahha wtf

--- project/random generatin/random.do
+++ project/random generatin/random.do
@@ -9,5 +9,6 @@ log {/*}
 add wave {/*}
 
 force {clock} 0 0,1 5 -r 10
+force {give_random} 0 0, 1 25 -r 50
 force {reset} 1 0,0 10
 run 20000ns
--- project/random generatin/random.v
+++ project/random generatin/random.v
@@ -1,23 +1,23 @@
 module rand (
+    input give_random,
     input clock,
     input reset,
-    output [12:0] rnd 
+    output [7:0] rnd 
     );
 
-reg [12:0] random, random_next, random_done;
-reg [3:0] count, count_next; //to keep track of the shifts
+reg [7:0] random, random_next, random_done;
 
-wire feedback = random[12] ^ random[3] ^ random[2] ^ random[0]; 
+wire feedback = random[7] ^ random[3] ^ random[2] ^ random[0]; 
  
 
 
  
 always @ (posedge clock)
 begin
- if (reset)
+ if (!reset)
  begin
-  random <= 13'hF; 
-  count <= 0;
+  random <= 8'hF; 
+
  end
   
  else
@@ -32,13 +32,13 @@ begin
  random_next = random; //default state stays the same
  count_next = count;
    
-  random_next = {random[11:0], feedback}; //shift left the xor'd every posedge clock
+  random_next = {random[6:0], feedback}; //shift left the xor'd every posedge clock
   count_next = count + 1;
  
- if (count == 13)
+ if (give_random)
  begin
   count = 0;
-  random_done = random; //assign the random number to output after 13 shifts
+  random_done = random; //assign the random number to output
  end
 end
 
