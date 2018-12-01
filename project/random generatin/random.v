242f2a7aa3d1a4774b4bd6b20dadcfcc9c531b1d
bowenzheng <dr.zheng49@gmail.com>
2018-12-01 00:33:02 -0500

    more mod

--- project/random generatin/random.v
+++ project/random generatin/random.v
@@ -12,7 +12,7 @@ assign linear_feedback = !(out[7] ^ out[3]);
 always @(posedge clock)
 begin
   if (!reset)
-    out <= 8'b0;
+    out <= 8'b01101011;
   else if (enable)
     begin
       out <= {out[6],out[5],out[4],out[3],out[2],out[1],out[0],linear_feedback};
