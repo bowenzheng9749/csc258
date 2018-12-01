dc208b3347032509b8f401093e10bf30076c19cd
bowenzheng <dr.zheng49@gmail.com>
2018-12-01 00:44:18 -0500

    more mod

--- project/random generatin/random.v
+++ project/random generatin/random.v
@@ -2,22 +2,24 @@ module random (enable, clock, reset, out);
 
 input clock, reset;
 input enable;
-output reg [7:0] out;
+output [7:0] out;
+reg [7:0] temp_out
 
 
 wire linear_feedback;
 
-assign linear_feedback = out[7] ^ out[3] ^ out[2] ^ out[0]; 
-always @(posedge enable)
+assign linear_feedback = temp_out[7] ^ temp_out[3] ^ temp_out[2] ^ temp_out[0]; 
+always @(posedge clock)
 begin
   if (!reset)
-    out <= 8'hF;
+    temp_out <= 8'hF;
   else if (enable)
     begin
-      out <= {out[6:0], linear_feedback};
+      temp_out <= {temp_out[6:0], linear_feedback};
     end
   
 end
+assign out = temp_out;
 
 
 endmodule
